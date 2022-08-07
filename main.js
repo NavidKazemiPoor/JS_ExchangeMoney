8ca96a2d123de39a573a559f15f3a9439f2bd52a
naweed kazemi <naweedkazemipoor@gmail.com>
2022-08-07 21:02:19 +0430

    initializing

---
+++ main.js
@@ -0,0 +1,38 @@
+const btnSwap = document.getElementById("swap");
+let amountOne = document.getElementById("amount-one");
+let amountTwo = document.getElementById("amount-two");
+let currencyOne = document.getElementById("currency-one");
+let currencyTwo = document.getElementById("currency-two");
+
+const fetchData = async () => {
+  const url = `https://open.exchangerate-api.com/v6/latest/${currencyOne.value}`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const { rates } = data;
+    calculate(rates[currencyTwo.value]);
+  } catch (error) {
+    console.log(error);
+  }
+};
+const calculate = (rate) => {
+  amountTwo.value = (amountOne.value * rate).toFixed(2);
+  
+};
+const changeSelect = () => {
+  fetchData();
+};
+currencyOne.addEventListener("change", changeSelect);
+currencyTwo.addEventListener('change',changeSelect);
+btnSwap.addEventListener('click',()=>{
+    let temp = 0;
+    temp = amountOne.value;
+    amountOne.value = amountTwo.value;
+    amountTwo.value = temp;
+});
+
+amountOne.addEventListener("input", () => {
+  fetchData();
+});
+
+fetchData();
